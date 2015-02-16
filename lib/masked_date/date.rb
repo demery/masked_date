2642d097ae878db99e22adf6183443105b955f4b
Doug Emery <emeryr@upenn.edu>
2015-02-16 11:42:30 -0500

    Change MaskedDate::Date initializer; test

--- lib/masked_date/date.rb
+++ lib/masked_date/date.rb
@@ -1,6 +1,6 @@
 module MaskedDate
   class Date
-    attr_reader :date, :mask
+    attr_accessor :date, :mask
     attr_accessor :year_format, :month_format, :day_format, :field_sep
 
     YEARBIT      = 1
@@ -11,11 +11,34 @@ module MaskedDate
     DAY_FORMAT   = 'd'
     FIELD_SEP    = '-'
 
-    # TODO: change to #initialize(date,mask); move current to
-    # helper/factory method
-    def initialize ymd={ y: nil, m: nil, d: nil, msk: nil }
+    # :method: intialize
+    #
+    # Create a new MaskedDate, based on the presence and absence of
+    # +:y+, +:m+, and +:d+ parameters.
+    #
+    # An integer +msk+ can be provided to override default mask
+    # creation.  +Msk+ is bit mask where YEAR = 1, MONTH = 2, and DAY
+    # = 4.
+    #
+    # === Parameters
+    #
+    # +ymd+ - A hash of +:y+, +:m+, +:d+ where the values represent
+    # year, month, and day, resp.
+    # +msk+ - A mask to override the default behavior
+    #
+    # === Examples
+    #
+    # MaskedDate::Date.new y: 1971, m: 3, d: 31 # => 1971-Mar-31
+    # MaskedDate::Date.new y: 1971, m: 3        # => 1971-Mar
+    # MaskedDate::Date.new y: 1971              # => 1971
+    # MaskedDate::Date.new m: 3, d: 31          # => Mar-31
+    #
+    # MaskedDate::Date.new { y: 1971, m: 3, d: 31 }, [ :year, :month ]
+    #                                           # => 1971-Mar
+    #
+    def initialize ymd={ y: nil, m: nil, d: nil }, msk=nil
       @date = ::Date.new (ymd[:y] || 0), (ymd[:m] || 1), (ymd[:d] || 1)
-      @mask = ymd[:msk]
+      @mask = msk
       if ! @mask
         @mask = 0
         @mask |= YEARBIT   if ymd[:y]
@@ -51,6 +74,10 @@ module MaskedDate
 
     def format_string
       formats.map{ |b,f| "%#{f}" if field_present?(b) }.compact.join field_sep
+    end
+
+    def to_a
+      [ year, month, day ]
     end
 
     def to_s
--- spec/masked_date_spec.rb
+++ spec/masked_date_spec.rb
@@ -5,7 +5,30 @@ describe MaskedDate do
     expect(MaskedDate::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context MaskedDate::Date do
+    it "creates a y-m-d date" do
+      expect(MaskedDate::Date.new(y: 1971, m: 3, d: 31).to_a).to eq([1971, 3, 31])
+    end
+
+    it "creates a y-m date" do
+      expect(MaskedDate::Date.new(y: 1971, m: 3).to_a).to eq([1971, 3, nil])
+    end
+
+    it "creates a y date" do
+      expect(MaskedDate::Date.new(y: 1971).to_a).to eq([1971, nil, nil])
+    end
+
+    it "creates a m-d date" do
+      expect(MaskedDate::Date.new(m: 3, d: 31).to_a).to eq([nil, 3, 31])
+    end
+
+    it "creates a y-m-d with m-d override mask" do
+      expect(MaskedDate::Date.new({y: 1971, m: 3, d: 31}, 6).to_a).to eq([nil, 3, 31])
+    end
+
+    it "creates a m-d with y-m-d override mask" do
+      expect(MaskedDate::Date.new({m: 3, d: 31}, 6).to_a).to eq([nil, 3, 31])
+    end
+
   end
 end
